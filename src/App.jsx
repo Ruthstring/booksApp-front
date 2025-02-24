9e85b6d4ae876dd348b91eab99af469963936bbd
Ruth Cuellar <ruthcllp@gmail.com>
2025-02-24 19:24:22 +0100

    basic installation completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="h-screen flex justify-center items-center bg-gray-500">
+  <h1 className="text-6xl font-bold text-blue-400">Tailwind colors working now!</h1>
+  <div className="h-18 w-6 bg-pink-500"></div>
+  
+</div>
+</>
+
+  )
+}
+
+export default App
